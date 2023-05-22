63e09b1251ef926c2f62b4d0a48f6e79003a09b6
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-22 17:30:16 +0200

    adding route entity

---
+++ src/Entity/Route.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RouteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RouteRepository::class)]
+class Route
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $departure = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $arrival = null;
+
+    #[ORM\Column]
+    private ?int $nbPlace = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $modelCar = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDeparture(): ?\DateTimeInterface
+    {
+        return $this->departure;
+    }
+
+    public function setDeparture(\DateTimeInterface $departure): self
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    public function getArrival(): ?\DateTimeInterface
+    {
+        return $this->arrival;
+    }
+
+    public function setArrival(\DateTimeInterface $arrival): self
+    {
+        $this->arrival = $arrival;
+
+        return $this;
+    }
+
+    public function getNbPlace(): ?int
+    {
+        return $this->nbPlace;
+    }
+
+    public function setNbPlace(int $nbPlace): self
+    {
+        $this->nbPlace = $nbPlace;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getModelCar(): ?string
+    {
+        return $this->modelCar;
+    }
+
+    public function setModelCar(string $modelCar): self
+    {
+        $this->modelCar = $modelCar;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
