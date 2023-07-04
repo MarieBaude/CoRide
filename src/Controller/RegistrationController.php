7a272111a64493941d107714a8412bba60e643c7
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-07-04 15:43:49 +0200

    add auth

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -24,17 +24,19 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
+            $formData = $form->getData();
+            $user->setFirstName($formData->getFirstName());
+            $user->setLastName($formData->getLastName());
+
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
-                    $form->get('plainPassword')->getData()
+                    $form->get('password')->getData()
                 )
             );
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
             return $userAuthenticator->authenticateUser(
                 $user,
